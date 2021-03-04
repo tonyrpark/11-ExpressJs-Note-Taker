9b322bac48d56536822f8499dfabf765bc80e250
tonyrpark <tonyrpark@gmail.com>
2021-03-04 15:03:50 -0800

    Routes.js complete, testing after editing index.js file

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -11,28 +11,45 @@ module.exports = (app) => {
 };
 
 // API Routes
-app.get("/api/notes",function(req,res){
-    res.json(notes); 
+app.get("/api/notes", function (req, res) {
+  res.json(notes);
 });
 
 // Setup the /api/notes post route
-app.post("/api/notes",function(req,res){
-    let newNote = req.body;
-    notes.push(newNote); 
-    updateDb(); 
-    return console.log("Added new note:" + newNote.title);
-}); 
+app.post("/api/notes", function (req, res) {
+  let newNote = req.body;
+  notes.push(newNote);
+  updateDb();
+  return console.log("Added new note:" + newNote.title);
+});
 
 // Retrieve note with specific ID
-app.get("/api/notes/:id",function(req,res){
-    res.json(notes[req.paramds.id]);
-}); 
+app.get("/api/notes/:id", function (req, res) {
+  res.json(notes[req.paramds.id]);
+});
 
 // Delete note
-app.delete("/api/notes/:id", function(req,res)
-{
-    notes.splice(req.paramds.id,1);
-    updateDb(); 
-    console.log("Deleted note with id" + req.paramds.id); 
+app.delete("/api/notes/:id", function (req, res) {
+  notes.splice(req.paramds.id, 1);
+  updateDb();
+  console.log("Deleted note with id" + req.paramds.id);
 });
-})
+
+// Display Notes.html
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// Display index.html
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+
+  // Update json file when note is added or deleted
+  function updateDb() {
+    fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
+      if (err) throw err;
+      return true;
+    });
+  }
+});
+}
