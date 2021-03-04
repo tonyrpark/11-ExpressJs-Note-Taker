3e7ecd2b7d68144df844a4a521035915c69fa246
tonyrpark <tonyrpark@gmail.com>
2021-03-04 14:35:46 -0800

    Server.js file progress

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,14 @@ const app = express();
 
 //If Line 10 does not work, try changing port numbers or change PORT to lowercase, 'port'.
 const PORT = process.env.PORT || 3000;
+
+//Data Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//Routes File
+require("./routes/routes")(app);
+
+//Listener
+app.listen(PORT, function () {});
