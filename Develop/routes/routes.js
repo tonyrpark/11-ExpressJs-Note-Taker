199e81a9429f89af09f8cf863fc75d648f30805c
tonyrpark <tonyrpark@gmail.com>
2021-03-04 14:55:53 -0800

    Added API routes, post, retrieve, and deletion of note code

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -9,3 +9,30 @@ module.exports = (app) => {
     var notes = JSON.parse(data);
   });
 };
+
+// API Routes
+app.get("/api/notes",function(req,res){
+    res.json(notes); 
+});
+
+// Setup the /api/notes post route
+app.post("/api/notes",function(req,res){
+    let newNote = req.body;
+    notes.push(newNote); 
+    updateDb(); 
+    return console.log("Added new note:" + newNote.title);
+}); 
+
+// Retrieve note with specific ID
+app.get("/api/notes/:id",function(req,res){
+    res.json(notes[req.paramds.id]);
+}); 
+
+// Delete note
+app.delete("/api/notes/:id", function(req,res)
+{
+    notes.splice(req.paramds.id,1);
+    updateDb(); 
+    console.log("Deleted note with id" + req.paramds.id); 
+});
+})
